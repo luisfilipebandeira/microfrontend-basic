bfd0f3e02f79e49e4c9778de3a6b0d998d1bc8e6
Luis Bandeira <luisbandeira@brq.com>
2024-11-22 08:42:21 -0300

    last commit basic microfrontend

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const itemsCart = `You have ${faker.random.number()} items in your cart`;
-
-document.querySelector('#dev-cart').innerHTML = itemsCart;
+import('./bootstrap')
